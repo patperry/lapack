f88c06141d09172860e0f9a54216a9501d727f20
Patrick Perry <patperry@stanford.edu>
2009-01-19 17:44:18 -0800

    export matrix classes

--- lib/Data/Matrix/House.hs
+++ lib/Data/Matrix/House.hs
@@ -16,6 +16,18 @@ module Data.Matrix.House (
     getReflector,
     setReflector,
     
+    -- * Overloaded interface for matrices
+    module Data.Matrix.Class,
+    module Data.Matrix.Class.IMatrix,
+    module Data.Matrix.Class.MMatrix,
+    module Data.Matrix.Class.ISolve,
+    module Data.Matrix.Class.MSolve,
     ) where
 
 import Data.Matrix.ReflectorBase
+
+import Data.Matrix.Class
+import Data.Matrix.Class.IMatrix
+import Data.Matrix.Class.MMatrix
+import Data.Matrix.Class.ISolve
+import Data.Matrix.Class.MSolve
